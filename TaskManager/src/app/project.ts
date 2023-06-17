138cbb6246d1fb307e3d0f5f4f9c87402a217e84
Talha <p.m.talha15@gmail.com>
2023-06-17 06:32:06 +0530

    Template forms
    Added radio buttons as well as New properties to support the client locations.

--- TaskManager/src/app/project.ts
+++ TaskManager/src/app/project.ts
@@ -1,13 +1,23 @@
+import { ClientLocation } from "./client-location";
+
 export class Project {
     projectID:string | any;
     projectName: string | any;
     dateOfStart: string | any;
     teamSize: string | any;
+    active: boolean | any;
+    status:string | any;
+    clientLocationID: number | any;
+    clientLocation: ClientLocation | any;
 
     constructor(){
         this.projectID = 0;
         this.projectName = null;
         this.dateOfStart = null;
         this.teamSize = 0;
+        this.active=true;
+        this.status=null;
+        this.clientLocationID=null;
+        this.clientLocation = new ClientLocation();
     }
 }
